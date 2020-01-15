04df79bf0d093967dc334407433885f9b8ab9405
MarcelinoRodrigues <marcelino.rodrigues.d.n@gmail.com>
2020-01-15 10:13:24 -0300

    uploads im manager my pag

--- src/Pages/Footer/footer.jsx
+++ src/Pages/Footer/footer.jsx
@@ -8,9 +8,9 @@ import Whatsapp from '../../assets/logo/whatsapp.png';
 const Footer = () => (
     <footer id="my-footer">
         <p>Colaborador</p>
-        <a href="https://github.com/MarcelinoRodrigues/playing-in-ReactJs.git" target="_blank"><img src={Git}/></a>
         <a href="https://www.instagram.com/ino_css/?hl=pt-br" target="_blank"><img src={Insta}/></a>
         <a href="https://whats.link/marcelinorodriguesdonascimento" target="_blank"><img src={Whatsapp}/></a>
+        <a href="https://github.com/MarcelinoRodrigues/playing-in-ReactJs.git" target="_blank"><img src={Git}/></a>
     </footer>
 );
 export default Footer;
--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 import Routes from './routes';
 import './Styles/main.css'
 
+alert("A pagina não está em funcionamento no momento");
+
 ReactDOM.render(<Routes />, document.getElementById('root'));

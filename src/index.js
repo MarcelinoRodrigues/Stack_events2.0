2e02ebe8f5db2aeb9188e31f8e436da2660c76e4
MarcelinoRodrigues <marcelino.rodrigues.d.n@gmail.com>
2020-01-23 23:46:02 -0300

    functionalidad from pag

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import ReactDOM from 'react-dom';
 import Routes from './routes';
 import './Styles/main.css'
 
-alert("A pagina não está em funcionamento no momento");
 
 ReactDOM.render(<Routes />, document.getElementById('root'));

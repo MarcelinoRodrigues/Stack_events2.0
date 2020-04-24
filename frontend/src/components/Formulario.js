458b0d96a2a07c540362458a2f38dc80b503cb25
MarcelinoRodrigues <marcelino.rodrigues.d.n@gmail.com>
2020-04-23 21:26:42 -0300

    modific arrow function because i am validation

--- frontend/src/components/Formulario.js
+++ frontend/src/components/Formulario.js
@@ -1,33 +1,36 @@
 import React from 'react';
 
-const Formulario =()=>(
-    <form id="form" method="POST" enctype="multipart/form-data">
-        <h2>Cadastrar Evento</h2>
-        <input 
-            type="text" 
-            id="nomeE" 
-            placeholder="Nome Do Evento" 
-            autocomplete="off"
-            required/>
-        <input 
-            type="date" 
-            id="dataE"
-            required/>
-        <input 
-            type="text"
-            id="descricao"
-            placeholder="Descrição do Evento"
-            autocomplete="off"
-            required/>
-        <span>
-            <h4>Arquivo</h4>
+function Formulario (){
+    
+    return(
+        <form id="form" method="POST" enctype="multipart/form-data">
+            <h2>Cadastrar Evento</h2>
             <input 
-                type="file" 
-                name="arquivo" 
-                id="arquivo"
+                type="text" 
+                id="nomeE" 
+                placeholder="Nome Do Evento" 
+                autocomplete="off"
                 required/>
-        </span>
-        <input type="submit" id="send" value="Criar"/>
-    </form>
-);
+            <input 
+                type="date" 
+                id="dataE"
+                required/>
+            <input 
+                type="text"
+                id="descricao"
+                placeholder="Descrição do Evento"
+                autocomplete="off"
+                required/>
+            <span>
+                <h4>Arquivo</h4>
+                <input 
+                    type="file" 
+                    name="arquivo" 
+                    id="arquivo"
+                    required/>
+            </span>
+            <input type="submit" id="send" value="Criar"/>
+        </form>
+    );
+}
 export default Formulario;
